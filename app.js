eb2b39afbf82a2917d6c840c970abff160c72363
ernsnl <ernsnl@gmail.com>
2016-09-04 20:27:06 +0300

    Update: Node modules were inserted

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
+var index = require('./app/index.js');
+var result = index.callResult();
 
 app.set('views', './views');
-app.use(express.static('public'));
 app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 
 app.get('/', function (req, res) {
   //res.send('Hello World!');
-  res.render('index', {message : 'Hello World'});
+  res.render('index', {message : result});
 });
 
 app.listen(3000, function () {
--- app/index.js
+++ app/index.js
@@ -2,11 +2,16 @@
 const calc = require('./calc');
 
 const numbersToAdd = [
-  3,
-  4,
-  10,
-  2
-]
+    3,
+    4,
+    10,
+    2
+];
 
-const result = calc.sum(numbersToAdd)
-console.log('The result is:' + result)
+function callResult() {
+    return calc.sum(numbersToAdd);
+}
+const result = calc.sum(numbersToAdd);
+console.log('The result is:' + result);
+
+module.exports.callResult = callResult;
