f631496d5774ccadb5d35bf83eaf28f97cd9ac2f
ernsnl <ernsnl@gmail.com>
2016-09-05 23:06:41 +0300

    Update: Bootstrap NavBar was integrated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('./app/firebase/fireBase.js');
+var firebaseFunc = require('./app/firebase/fireBase.js');
 
 
 var express = require('express');
@@ -7,7 +7,7 @@ var app = express();
 var index = require('./app/index.js');
 var result = index.callResult();
 
-app.set('views', './views');
+app.set('views', ['./views', './views/home']);
 app.set('view engine', 'pug');
 
 app.use('/lib', express.static('bower_components'));
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   //res.send('Hello World!');
-  res.render('index', {message : result});
+  res.render('index', {message : result, links: ['aaa', 'bbb']});
 });
 
 app.listen(3000, function () {
--- public/src/firebase_auth.js
+++ public/src/firebase_auth.js
@@ -1,9 +1,13 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+
+/*
 firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+
+
   // ...
 }).catch(function(error) {
   // Handle Errors here.
@@ -15,3 +19,4 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   var credential = error.credential;
   // ...
 });
+*/
