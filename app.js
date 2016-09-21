9352444e761b5b6d66a312eca47d6ceeae0df329
ernsnl <ernsnl@gmail.com>
2016-09-21 22:06:29 +0300

    Update: App.yaml

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get('/getSearchResults', function(req, res) {
     }
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || '3000', function() {
     console.log('Example app listening on port 3000!');
 });
